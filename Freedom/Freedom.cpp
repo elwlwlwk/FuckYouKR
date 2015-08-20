9eef67b4231c998a13d82a197919450e01a88782
wisewolf <holo@wisewolf.org>
2015-08-21 03:17:18 +0900

    Fixed some bugs. Bugs when send POST and so on.

--- Freedom/Freedom.cpp
+++ Freedom/Freedom.cpp
@@ -7,18 +7,15 @@
 #include <ws2tcpip.h>
 #include <string>
 #include <regex>
-#include <http.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 std::vector<std::string> feed_phost(std::string s_phost) {
 	std::string phost = "";
 	int breakpoint = rand() % (s_phost.length() - 1);
-	return {"Host: "+s_phost.substr(0, breakpoint), s_phost.substr(breakpoint, s_phost.length() - breakpoint)+"\r\n"};
+	return{ "Host: " + s_phost.substr(0, breakpoint) + s_phost.substr(breakpoint, s_phost.length() - breakpoint) + "\r\n" };
 }
 std::string generate_dummyheaders() {
-	
-
 	std::string dummy_header = "";
 	for (int l = 0; l < 32; l++) {
 		std::string dummy_key = "X-", dummy_value = "";
@@ -54,6 +51,21 @@ std::vector<std::string> tokenize(const std::string& str,
 	return tokens;
 }
 
+std::string trim(std::string const& str)
+{
+	std::size_t first = str.find_first_not_of(' ');
+
+	// If there is no non-whitespace character, both first and last will be std::string::npos (-1)
+	// There is no point in checking both, since if either doesn't work, the
+	// other won't work, either.
+	if (first == std::string::npos)
+		return "";
+
+	std::size_t last = str.find_last_not_of(' ');
+
+	return str.substr(first, last - first + 1);
+}
+
 int validateArgs(int argc, char* argv[]) {
 	if (argc > 3) {
 		return -1;
@@ -65,8 +77,11 @@ int validateArgs(int argc, char* argv[]) {
 	return 0;
 }
 
-std::vector<std::string> generate_new_header(const std::string header) {
-	std::vector<std::string> v_header = tokenize(header, "\r\n");
+bool handle_https(const std::vector<std::string> v_header) {
+	return true;
+}
+
+std::vector<std::string> generate_new_header(const std::vector<std::string> v_header) {
 	std::vector<std::string> v_head = tokenize(v_header[0]);
 	std::string s_phost;
 	std::vector<std::string> v_sreq;
@@ -89,16 +104,13 @@ std::vector<std::string> generate_new_header(const std::string header) {
 			v_sreq.push_back(v_header[i]);
 		}
 	}
-	if (!std::regex_search(header, std::regex("[Cc][Oo][Nn][Nn][Ee][Cc][Tt][Ii][Oo][Nn]"))) {
-		v_sreq.push_back("Connection: close");
-	}
 	if (s_phost.length() == 0) {
 		s_phost = "127.0.0.1";
 	}
 	std::string s_path = v_head[1].substr(s_phost.length() + 7);
 	std::string s_new_head = v_head[0] + " " + s_path + " " + v_head[2];
 	std::vector<std::string> v_new_header;
-	
+
 	v_new_header.push_back(s_new_head + "\r\n");
 	v_new_header.push_back(generate_dummyheaders());
 	std::vector<std::string> phost = feed_phost(s_phost);
@@ -110,41 +122,39 @@ std::vector<std::string> generate_new_header(const std::string header) {
 	return v_new_header;
 }
 
-std::vector<std::string> process_new_host(const std::string header) {
-	std::vector<std::string> v_header = tokenize(header, "\r\n");
+std::vector<std::string> process_new_host(const std::vector<std::string> v_header) {
 	std::string s_phost;
 	std::string s_new_host, s_new_port;
 
 	for (int i = 1; i < v_header.size(); i++) {
-		std::vector<std::string> v_line = tokenize(v_header[i], ": ");
-
-		if (std::regex_search(v_line[0], std::regex("[Hh][Oo][Ss][Tt]"))) {
-			s_phost = v_line[1];
+		if (std::regex_search(v_header[i], std::regex("[Hh][Oo][Ss][Tt]"))) {
+			std::vector<std::string> v_line = tokenize(v_header[i], ": ");
+			s_phost = v_header[i];
+			if (std::regex_search(s_phost, std::regex("(.+?):([0-9]{1,5})"))) {
+				s_new_host = tokenize(s_phost, ":")[1];
+				s_new_port = tokenize(s_phost, ":")[2];
+			}
+			else {
+				s_new_host = tokenize(s_phost, ":")[1];
+				s_new_port = "80";
+			}
 		}
 	}
-	if (std::regex_search(s_phost, std::regex("(.+?):([0-9]{1,5})"))) {
-		s_new_host = tokenize(s_phost, ":")[0];
-		s_new_port = tokenize(s_phost, ":")[1];
-	}
-	else {
-		s_new_host = s_phost;
-		s_new_port = "80";
-	}
-	return { s_new_host,s_new_port };
+	return{ trim(s_new_host),trim(s_new_port) };
 }
 
 int throw_response(const SOCKET ProxySocket, const SOCKET ClientSocket) {
 	int iResult;
 	char recvbuf[1024];
 	int recvbuflen = sizeof(recvbuf);
-	int content_length;
+	int content_length = 0;
 	iResult = recv(ProxySocket, recvbuf, recvbuflen, 0);
 	std::smatch m;
 	std::string s_recvdata = std::string(recvbuf, iResult);
 	std::regex_search(s_recvdata, m, std::regex("\r\n\r\n"));
-	std::string s_header = s_recvdata.substr(0, m.position(0)+4);
+	std::string s_header = s_recvdata.substr(0, m.position(0) + 4);
 	std::vector<std::string> v_header = tokenize(s_header, "\r\n");
-	std::string s_data = s_recvdata.substr(m.position(0)+4);
+	std::string s_data = s_recvdata.substr(m.position(0) + 4);
 	int recv_len = s_data.length();
 	for (int i = 0; i < v_header.size(); i++) {
 		if (std::regex_search(v_header[i], std::regex("[Cc][Oo][Nn][Tt][Ee][Nn][Tt]-[Ll][Ee][Nn][Gg][Tt][Hh]"))) {
@@ -153,7 +163,8 @@ int throw_response(const SOCKET ProxySocket, const SOCKET ClientSocket) {
 	}
 	send(ClientSocket, s_header.c_str(), s_header.length(), 0);
 	send(ClientSocket, s_data.c_str(), s_data.length(), 0);
-	while (recv_len<content_length || iResult == recvbuflen) {
+	ZeroMemory(recvbuf, recvbuflen);
+	while (recv_len < content_length || !std::regex_search(recvbuf, std::regex("\r\n"))) {
 		ZeroMemory(recvbuf, recvbuflen);
 		iResult = recv(ProxySocket, recvbuf, recvbuflen, 0);
 		if (iResult > 0) {
@@ -166,70 +177,80 @@ int throw_response(const SOCKET ProxySocket, const SOCKET ClientSocket) {
 			continue;
 		}
 		iResult = send(ClientSocket, recvbuf, iResult, 0);
-	} 
+	}
 	return 0;
+}
+
+bool validate_header(std::string header) {
+	if (header.size() == 0) {
+		return false;
+	}
+	std::vector<std::string> v_header = tokenize(header, "\r\n");
+	if (v_header.size() < 4) {
+		return false;
+	}
 }
 
 int main(int argc, char* argv[])
 {
 
-		srand(time(0));
-		validateArgs(argc, argv);
+	srand(time(0));
+	validateArgs(argc, argv);
 
-		WSADATA wsaData;
-		int iResult;
+	WSADATA wsaData;
+	int iResult;
 
-		// Initialize Winsock
-		iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-		if (iResult != 0) {
-			std::cout << "WSAStartup failed: " << iResult << std::endl;
-			return 1;
-		}
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0) {
+		std::cout << "WSAStartup failed: " << iResult << std::endl;
+		return 1;
+	}
 
-		struct addrinfo *result = NULL, *ptr = NULL, hints;
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
 
-		ZeroMemory(&hints, sizeof(hints));
-		hints.ai_family = AF_INET;
-		hints.ai_socktype = SOCK_STREAM;
-		hints.ai_protocol = IPPROTO_TCP;
-		hints.ai_flags = AI_PASSIVE;
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
 
-		// Resolve the local address and port to be used by the server
-		iResult = getaddrinfo(NULL, argv[2], &hints, &result);
-		if (iResult != 0) {
-			std::cout << "getaddrinfo failed: " << iResult << std::endl;
-			WSACleanup();
-			return 1;
-		}
-		SOCKET ListenSocket = INVALID_SOCKET;
-		ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-		if (ListenSocket == INVALID_SOCKET) {
-			std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
-			freeaddrinfo(result);
-			WSACleanup();
-			return 1;
-		}
-
-		iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-		if (iResult == SOCKET_ERROR) {
-			std::cout << "bind failed with error: " << WSAGetLastError() << std::endl;
-			freeaddrinfo(result);
-			closesocket(ListenSocket);
-			WSACleanup();
-			return 1;
-		}
-
+	// Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, argv[2], &hints, &result);
+	if (iResult != 0) {
+		std::cout << "getaddrinfo failed: " << iResult << std::endl;
+		WSACleanup();
+		return 1;
+	}
+	SOCKET ListenSocket = INVALID_SOCKET;
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET) {
+		std::cout << "Error at socket(): " << WSAGetLastError() << std::endl;
 		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
 
-		if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
-			std::cout << "Listen failed with error: " << WSAGetLastError() << std::endl;
-			closesocket(ListenSocket);
-			WSACleanup();
-			return 1;
-		}
-		SOCKET ClientSocket = INVALID_SOCKET;
-		// Accept a client socket
-		while (1) {
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR) {
+		std::cout << "bind failed with error: " << WSAGetLastError() << std::endl;
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	freeaddrinfo(result);
+
+	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR) {
+		std::cout << "Listen failed with error: " << WSAGetLastError() << std::endl;
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	SOCKET ClientSocket = INVALID_SOCKET;
+	// Accept a client socket
+	while (1) {
 		ClientSocket = accept(ListenSocket, NULL, NULL);
 		if (ClientSocket == INVALID_SOCKET) {
 			std::cout << "accept failed: " << WSAGetLastError() << std::endl;
@@ -264,12 +285,38 @@ int main(int argc, char* argv[])
 
 		} while (!std::regex_search(header, std::regex("\r\n\r\n")));
 		ZeroMemory(recvbuf, sizeof(recvbuf));
-		if (header.size() == 0) {
+		std::vector<std::string> v_new_header;
+		std::vector<std::string> s_new_host;
+		if (!validate_header(header)) {
 			continue;
 		}
-		std::vector<std::string> v_new_header = generate_new_header(header);
-		std::vector<std::string> s_new_host = process_new_host(header);
-		
+		std::vector<std::string> v_header = tokenize(header, "\r\n");
+		std::string content;
+		for (int i = 0; i < v_header.size(); i++) {
+			if (std::regex_search(v_header[i], std::regex("Content-Length"))) {
+				int content_length = atoi(trim(tokenize(v_header[i], ":")[1]).c_str());
+				if (iResult < sizeof(recvbuf)) {
+					std::smatch m;
+					std::regex_search(header, m, std::regex("\r\n\r\n"));
+					content = header.substr(m.position() + 4)+"\r\n";
+					header = header.substr(0, m.position() + 4);
+				}
+			}
+		}
+		v_header = tokenize(header, "\r\n");
+		if (std::regex_search(v_header[0], std::regex("[Cc][Oo][Nn][Nn][Ee][Cc][Tt]"))) {
+			handle_https(v_header);
+			closesocket(ClientSocket);
+			continue;
+			/*v_new_header = v_header;
+			s_new_host = process_new_host(v_header);
+			send(ClientSocket, "HTTP/1.1 200 Connection established\r\n\r\n", 40, 0);*/
+		}
+		else {
+			v_new_header = generate_new_header(v_header);
+			s_new_host = process_new_host(v_header);
+		}
+
 		//gethostbyname(s_new_host.c_str());
 		iResult = getaddrinfo(s_new_host[0].c_str(), s_new_host[1].c_str(), &hints, &result);
 		if (iResult != 0) {
@@ -298,6 +345,7 @@ int main(int argc, char* argv[])
 		for (int i = 0; i < v_new_header.size(); i++) {
 			iResult = send(ProxySocket, v_new_header[i].c_str(), v_new_header[i].length(), 0);
 		}
+		iResult = send(ProxySocket, content.c_str(), content.length(), 0);
 		if (iResult == SOCKET_ERROR) {
 			printf("send failed: %d\n", WSAGetLastError());
 			closesocket(ProxySocket);
@@ -308,5 +356,5 @@ int main(int argc, char* argv[])
 		iResult = throw_response(ProxySocket, ClientSocket);
 		closesocket(ProxySocket);
 	}
-    return 0;
+	return 0;
 }
